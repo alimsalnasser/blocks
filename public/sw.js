75bd670f14e313dbb98892bccf0c8a2ee7f679b1
alialnasser <32553537+alimsalnasser@users.noreply.github.com>
2026-06-03 15:48:56 +0300

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "barricade-v10";
+const CACHE = "barricade-v11";
 const ASSETS = ["/", "/index.html", "/style.css", "/app.js", "/manifest.json", "/icon.svg"];
 
 self.addEventListener("install", e => {
